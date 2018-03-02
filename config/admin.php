297e7feece8907684d29d06e18234040c2976e31
qjd <qjd@163.com>
2018-03-02 14:30:43 +0800

    4

--- config/admin.php
+++ config/admin.php
@@ -5,17 +5,17 @@ return [
     /*
      * Laravel-admin name.
      */
-    'name'      => 'Laravel-admin',
+    'name'      => 'test-admin',
 
     /*
      * Logo in admin panel header.
      */
-    'logo'      => '<b>Laravel</b> admin',
+    'logo'      => '<b>test</b> admin',
 
     /*
      * Mini-logo in admin panel header.
      */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>T</b>',
 
     /*
      * Laravel-admin url prefix.
