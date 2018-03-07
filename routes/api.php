482fbeaea3bddcf4d8d359b36330b84b51e37487
qjd <qjd@163.com>
2018-03-07 14:22:48 +0800

    111

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::any('webhook',function(Request $request){
-	//  file_get_contents('php://input');//
+	//  file_get_contents('php://input');2
 	$res[] = shell_exec('whoami');
 	$res[] = shell_exec('cd /www/bill_admin && git pull');
 	\Log::info($res);
